aa44d1a6b891dfec06e267d9dcb0a3bc63ae47fd
Gayuh Ridho <gayuhridho369@gmail.com>
2024-09-22 13:12:52 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
-            ssr: 'resources/js/ssr.ts',
+            input: "resources/js/app.ts",
+            ssr: "resources/js/ssr.ts",
             refresh: true,
+            server: {
+                https: process.env.APP_ENV === "production",
+            },
         }),
         vue({
             template: {
