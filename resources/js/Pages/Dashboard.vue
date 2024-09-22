e336d18581cdb528fb193652577e92a2b4ee209d
Gayuh Ridho <gayuhridho369@gmail.com>
2024-09-22 17:16:38 +0700

    try prime vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
 </script>
 
 <template>
+
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
@@ -14,6 +17,18 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <Button label="Hello World" />
+                    <Card>
+                        <template #title>Simple Card</template>
+                        <template #content>
+                            <p class="m-0">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
+                                error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis,
+                                culpa ratione quam perferendis esse, cupiditate neque
+                                quas!
+                            </p>
+                        </template>
+                    </Card>
                     <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
                 </div>
             </div>
